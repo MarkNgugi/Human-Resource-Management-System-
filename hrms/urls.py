03782e1c38959c7e4f3c5954f33900f3a623e819
mark <mmarkmarkm0@gmail.com>
2024-11-05 22:05:03 +0300

    employee management templates added:

--- hrms/urls.py
+++ hrms/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
     path('hr_manager_dashboard/', hr_manager_dashboard, name='hr_manager_dashboard'),
     path('employee_dashboard/', employee_dashboard, name='employee_dashboard'),
+
+    path('employee-profile/',employee_profile,name = 'employeeprofile'),
+    path('new-employee',new_employee,name='newemployee'),
+    path('document_management',document_management,name='documentmanagement'),
+    path('offboarding',offboarding,name='offboarding')
 ]
--- hrms/views.py
+++ hrms/views.py
@@ -14,9 +14,76 @@ def dashboard(request):
     else:
         return redirect('login')
 
+# ================================================================================================
+                                        # ADMIN START
+# ================================================================================================
+
 @login_required
 def admin_dashboard(request):
     return render(request, 'hrms/admin/admin_dashboard.html')
+ 
+# ============================EMPLOYEE MANAGEMENT START============================
+def employee_profile(request):
+    context = {}
+    return render(request,'hrms/admin/employee-management/employee_profile.html',context)
+
+def new_employee(request):
+    context={}
+    return render(request,'hrms/admin/employee-management/new_employee.html',context)
+
+def document_management(request):
+    context={}
+    return render(request,'hrms/admin/employee-management/document_management.html',context)
+
+def offboarding(request):
+    context={}
+    return render(request,'hrms/admin/employee-management/offboarding.html',context)
+
+# ============================EMPLOYEE MANAGEMENT END==============================
+
+# ============================HIRING AND ONBOARDING START============================
+
+def job_posting(request):
+    pass
+
+def application_tracking(request):
+    pass
+
+def interview_scheduling(request):
+    pass
+
+def onboarding_workflow(request):
+    pass
+# ============================HIRING AND ONBOARDING END==============================
+
+# ============================ATTENDANCE AND LEAVE START============================
+
+def time_tracking(request):
+    pass
+
+# ============================ATTENDANCE AND LEAVE END==============================
+
+# ============================REPORTS START=========================================
+
+def report_builder(request):
+    pass
+
+# ============================REPORTS END===========================================
+
+# ============================SETTINGS START=========================================
+
+def system_config(request):
+    pass
+
+# ============================SETTINGS END===========================================
+
+
+# ================================================================================================
+                                        # ADMIN END
+# ================================================================================================
+
+
+
 
 @login_required
 def hr_manager_dashboard(request):
@@ -25,3 +92,4 @@ def hr_manager_dashboard(request):
 @login_required
 def employee_dashboard(request):
     return render(request, 'hrms/employee/employee_dashboard.html')
+
