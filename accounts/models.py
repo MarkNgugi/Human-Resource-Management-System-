e285ee396f89eefc0cb351ec38accba954254d87
mark <mmarkmarkm0@gmail.com>
2025-03-05 11:36:15 +0300

    template changes:

--- accounts/models.py
+++ accounts/models.py
@@ -24,7 +24,7 @@ class CustomUser(AbstractUser):
     gender = models.CharField(max_length=20, choices=[('male', 'Male'), ('female', 'Female')], default='male')
     phone = models.CharField(max_length=15, blank=True, null=True)
     position = models.CharField(max_length=100, blank=True, null=True)
-    department = models.ForeignKey('Department', on_delete=models.CASCADE, default=None, null=True)
+    # department = models.ForeignKey('Department', on_delete=models.CASCADE, default=None, null=True)
     start_date = models.DateField(blank=True, null=True)
     exit_date = models.DateField(null=True, blank=True)
     reason_for_leaving = models.CharField(max_length=255, null=True, blank=True)
